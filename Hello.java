bb5d467a4a281cddb54273df3296debb78fd3b7d
ayang2004 <ayansnehgangradegangrade@gmail.com>
2023-07-06 18:14:57 +0530

    save

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package co.in.rays;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		String hello=  ("this is my beautyfull world");
+		System.out.println(hello.charAt(5));
+		System.out.println(hello);
+	}
+}
